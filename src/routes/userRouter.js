9d3960bbf3174143ce88634389915096559a3d56
Alejandra Massuh <mmass004@fiu.edu>
2018-11-03 15:22:46 -0400

    began user route

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,1 +1,30 @@
-userRouter.js
+//CRUD
+var express = require('express');
+var routers = express.Router();
+
+var user = require('../models/user');
+
+// create new user
+userRouter.route('/add/post').post(function (req, res) {
+
+  var user = new User(req.body);
+      user.save()
+    .then(user => {
+    res.json(user);
+    })
+    .catch(err => {
+    res.status(400).send("unable to save to database");
+    });
+});
+
+
+// update info
+
+
+// update points
+
+
+// update favorite
+
+
+// change password
