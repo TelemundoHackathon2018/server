8eae4db500470ca5c36b499ad2fc922447af5825
Alejandra Massuh <mmass004@fiu.edu>
2018-11-03 14:49:51 -0400

    add user model and route along with server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var path = require('path');
+var cors = require('cors');
+var mongoose = require('mongoose');
+
+var port = 4200;
+
+mongoose.Promise = require('bluebird');
+mongooe.connect('mongodb://nbsee-admin:fiunbcu2018@nb-see-shard-00-00-1gn1a.gcp.mongodb.net:27017,nb-see-shard-00-01-1gn1a.gcp.mongodb.net:27017,nb-see-shard-00-02-1gn1a.gcp.mongodb.net:27017/test?ssl=true&replicaSet=NB-SEE-shard-0&authSource=admin&retryWrites=true')
+    .then(() => {
+        console.log("Start");
+    })
+    .catch(err => {
+        console.error("Starting Error: ", err.stack);
+        process.exit(1);
+    });
+    
