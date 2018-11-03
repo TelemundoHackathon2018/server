836b5b23b9b7fec27bcaa8673190ab4b50a3c281
Maria-Jose Ramos <mjramos@Maria-Joses-MacBook-Pro.local>
2018-11-03 15:02:43 -0400

    adding user schema

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,20 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var User = new Schema({});
+var User = new Schema({
+    "firstName": String,
+    "lastName": String,
+    "points": Number,
+    "favorites": Array,
+    "email": String,
+    "password": String,
+    "userName": String,
+    "social": {
+        "facebook": Schema.Types.Mixed,
+        "google": Schema.Types.Mixed,
+        "twitter": Schema.Types.Mixed,
+        "instagram": Schema.Types.Mixed,
+    }
+});
 
 module.export = mongoose.model("User", User);
